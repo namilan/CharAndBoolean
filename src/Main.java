62dbbc7bbb33f5a0d377be51d0a4b0035a30a6a4
namilan <nichole.milan@gmail.com>
2020-04-02 15:40:00 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-
+		char myChar = 'D';
+		char myUnicodeChar = '\u0044';
+		System.out.println(myChar);
+		System.out.println(myUnicodeChar);
 	}
 
 }
